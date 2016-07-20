7692043392e6bd647cb82e458874bccf4f63f6d9
Eduardo Palomino <Eduardo@Mac-mini-de-DOAPPS-2.local>
2016-07-20 17:41:49 -0500

    Integraciones Cocoapods, alamofire, swiftyjson

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'RestClientDemo' do
+  pod 'Alamofire', '~> 3.4'
+  pod 'GuillotineMenu', '~> 2.0.2'
+  pod 'SwiftyJSON'
+end
---
+++ Pods/Target Support Files/Pods-RestClientDemo/Pods-RestClientDemo-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RestClientDemoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RestClientDemoVersionString[];
+
